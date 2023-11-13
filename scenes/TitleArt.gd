17a2ebe70809f9580b8c6f13ec68f38ca423d0e6
wu-json <jason.wu2153@gmail.com>
2023-11-12 20:03:06 -0500

    feat: center

---
+++ scenes/TitleArt.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+
+func _ready():
+	var viewport_size = get_viewport_rect().size
+	position = Vector2((viewport_size.x / 2), (viewport_size.y / 2))
+
+func _process(_delta):
+	pass
