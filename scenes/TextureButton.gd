a656bf5ff21f75bfc2243f5ea6584fb50fd606e6
wu-json <jason.wu2153@gmail.com>
2023-11-12 20:44:35 -0500

    feat: scene 1

--- scenes/TextureButton.gd
+++ scenes/TextureButton.gd
@@ -1,9 +1,8 @@
-extends Button
+extends TextureButton
 
 func _ready():
-	print("Ready")
-	pass
+	var viewport_size = get_viewport_rect().size
+	position = Vector2((viewport_size.x / 2) - (self.get_rect().size.x / 2), (viewport_size.y - 150))
 	
-func _button_pressed():
-	print("PRESSED YOU")
-	pass
+func _pressed():
+	print("Play button was pressed")
