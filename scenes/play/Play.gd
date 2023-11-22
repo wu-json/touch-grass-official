a1a5907428a367097e99b4d3f416baab71614e5c
wu-json <jason.wu2153@gmail.com>
2023-11-22 15:12:20 -0500

    feat: update tree

--- scenes/play/Play.gd
+++ scenes/play/Play.gd
@@ -8,6 +8,9 @@ func on_click_mob(points):
 	score += points
 	$ScoreCounter.set_text(str(score))
 	_spawn_random_mob()
+	
+func on_timer_end():
+	get_tree().change_scene_to_file("res://scenes/Score/Score.tscn")
 
 func _spawn_random_mob():
 	var viewport_size = get_viewport_rect().size
