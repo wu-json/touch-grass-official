f49cfc949cd657c9c756b0c6f03613e4c0942b4e
wu-json <jason.wu2153@gmail.com>
2023-11-22 15:39:02 -0500

    feat: score display

--- scenes/Score/Score.gd
+++ scenes/Score/Score.gd
@@ -1,4 +1,6 @@
 extends Node2D
 
 func _ready():
-	$ScoreLabel.set_text("lol")
+	var score_param = SceneSwitcher.get_param("score")
+	var score: int = score_param if score_param != null else 69420
+	$ScoreLabel.set_text(str(score))
