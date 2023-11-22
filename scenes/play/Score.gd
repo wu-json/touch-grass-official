ee5b491418f00c6896578f27a98eb9a0fb8dd3f1
wu-json <jason.wu2153@gmail.com>
2023-11-21 19:01:37 -0500

    feat: score state

--- scenes/play/Score.gd
+++ scenes/play/Score.gd
@@ -2,10 +2,8 @@ extends Label
 
 func _ready():
 	var viewport_size = get_viewport_rect().size
-	position = Vector2(viewport_size.x - 120, 20)
+	position = Vector2(viewport_size.x - 100, 20)
 	add_theme_font_size_override("font_size", 30)
-	set_text("hello")
-	pass
 
 func _process(delta):
 	pass
--- scenes/play/play.gd
+++ scenes/play/play.gd
@@ -3,6 +3,7 @@ extends Node2D
 var score = 0;
 
 func _ready():
+	$ScoreCounter.set_text(str(score))
 	pass
 
 func _process(delta):
