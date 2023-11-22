3c948c305d38b0e671c2406f31d1ae8a2ba63a19
wu-json <jason.wu2153@gmail.com>
2023-11-21 19:33:59 -0500

    feat: make into func

--- scenes/play/Play.gd
+++ scenes/play/Play.gd
@@ -4,12 +4,15 @@ var grass_mob = preload("res://scenes/Play/GrassMob.tscn")
 
 var score = 0;
 
-func _ready():
-	$ScoreCounter.set_text(str(score))
+func spawn_random_mob():
 	var viewport_size = get_viewport_rect().size
 	var start_mob = grass_mob.instantiate()
 	start_mob.position = Vector2(randi_range(0, viewport_size.x), randi_range(0, viewport_size.y - 120))
 	add_child(start_mob)
+
+func _ready():
+	$ScoreCounter.set_text(str(score))
+	spawn_random_mob()
 
 func _process(delta):
 	pass
