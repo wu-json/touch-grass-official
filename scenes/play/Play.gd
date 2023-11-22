df3d2e0bebc001a39eb8694269c44d4c5e286cc1
wu-json <jason.wu2153@gmail.com>
2023-11-21 19:30:04 -0500

    feat: random spawning

--- scenes/play/Play.gd
+++ scenes/play/Play.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
+var grass_mob = preload("res://scenes/Play/GrassMob.tscn")
+
 var score = 0;
 
 func _ready():
 	$ScoreCounter.set_text(str(score))
-	pass
+	var start_mob = grass_mob.instantiate()
+	start_mob.position = Vector2(randi_range(10,1000), randi_range(10,100))
+	add_child(start_mob)
 
 func _process(delta):
 	pass
