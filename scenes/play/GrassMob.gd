16d03aacdf5b8ccb0e4b8d564e22c09692a02192
wu-json <jason.wu2153@gmail.com>
2023-11-21 19:26:21 -0500

    feat: move mob to scene

--- scenes/play/GrassMob.gd
+++ scenes/play/GrassMob.gd
@@ -2,8 +2,6 @@ extends Sprite2D
 
 func _ready():
 	self.scale = Vector2(0.5, 0.5)
-	var viewport_size = get_viewport_rect().size
-	position = Vector2((viewport_size.x / 2), (viewport_size.y / 2))
 
 func _process(delta):
 	pass
