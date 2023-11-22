641cfd11680c67810f29fff5b1d9c81778cfb158
wu-json <jason.wu2153@gmail.com>
2023-11-21 21:10:01 -0500

    feat: rename signal

--- scenes/play/Area2D.gd
+++ scenes/play/Area2D.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-signal pressed(points)
+signal grass_pressed(points)
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton \
 	and event.button_index == MOUSE_BUTTON_LEFT \
 	and event.is_pressed():
-		pressed.emit(1)
+		grass_pressed.emit(1)
 		get_parent().queue_free()
 
 func _ready():
