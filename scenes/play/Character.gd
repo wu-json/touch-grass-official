0f5491c3e7af317d4bf76643c21885f40bf25ddc
wu-json <jason.wu2153@gmail.com>
2023-11-23 18:12:08 -0500

    feat: remove scripts

--- scenes/play/Character.gd
+++
@@ -1,5 +0,0 @@
-extends Sprite2D
-
-func _ready():
-	var viewport_size = get_viewport_rect().size
-	position = Vector2((viewport_size.x / 2), (viewport_size.y - (self.get_rect().size.y / 2)))
--- scenes/play/Score.gd
+++
@@ -1,4 +0,0 @@
-extends Label
-
-func _ready():
-	add_theme_font_size_override("font_size", 30)
