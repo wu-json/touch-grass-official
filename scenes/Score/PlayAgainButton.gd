cf3132f76e3059634d4e9a030188c719b977a6ad
wu-json <jason.wu2153@gmail.com>
2023-11-23 18:46:00 -0500

    feat: play again button

--- scenes/Score/PlayAgainButton.gd
+++ scenes/Score/PlayAgainButton.gd
@@ -2,3 +2,12 @@ extends TextureButton
 
 func _pressed():
 	SceneSwitcher.change_scene("res://scenes/Play/Play.tscn")
+
+
+func _on_mouse_entered():
+	self.scale = Vector2(1.02, 1.05)
+	self.modulate = Color(1, 0.9, 1 , 1) 
+
+func _on_mouse_exited():
+	self.scale = Vector2(1.0, 1.0)
+	self.modulate = Color(1, 1, 1 , 1) 
