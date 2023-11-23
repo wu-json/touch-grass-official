3fd0c10aceb8d1896fcbc88fd0698974c9e1696e
wu-json <jason.wu2153@gmail.com>
2023-11-23 17:47:18 -0500

    feat: 60 sec timer

--- scenes/play/GameTimer.gd
+++ scenes/play/GameTimer.gd
@@ -1,12 +1,12 @@
 extends Timer
 
-const game_time_seconds = 10
+const GAME_TIME_SECONDS = 60
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.start(game_time_seconds)
+	self.start(GAME_TIME_SECONDS)
 	self.set_one_shot(true)
-	$GameTimerLabel.set_text(str(game_time_seconds))
+	$GameTimerLabel.set_text(str(GAME_TIME_SECONDS))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
