2b6ba42e48a2994cb69b2f03fa6b7bd6a34fa497
wu-json <jason.wu2153@gmail.com>
2023-11-23 13:04:48 -0500

    feat: play button

--- scenes/welcome/TextureButton.gd
+++ scenes/welcome/TextureButton.gd
@@ -2,7 +2,8 @@ extends TextureButton
 
 func _ready():
 	var viewport_size = get_viewport_rect().size
-	position = Vector2((viewport_size.x / 2) - (self.get_rect().size.x / 2), (viewport_size.y - 150))
+	var self_size = self.get_rect().size
+	position = Vector2(viewport_size.x - self_size.x + 20, viewport_size.y - self_size.y + 40)
 	
 func _pressed():
 	SceneSwitcher.change_scene("res://scenes/Play/Play.tscn")
