8bfd0ed3039d3554169ced334bde9acf2b693b0e
wu-json <jason.wu2153@gmail.com>
2023-11-23 18:17:27 -0500

    feat: update timer

--- scenes/play/GameTimer.gd
+++ scenes/play/GameTimer.gd
@@ -1,14 +1,13 @@
 extends Timer
 
-const GAME_TIME_SECONDS = 20
+const GAME_TIME_SECONDS = 120
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.start(GAME_TIME_SECONDS)
 	self.set_one_shot(true)
-	$GameTimerLabel.set_text(str(GAME_TIME_SECONDS))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	$GameTimerLabel.set_text(str(int(self.time_left)))
+	$GameTimerLabel.set_text(str(int(self.time_left)) + " sec")
