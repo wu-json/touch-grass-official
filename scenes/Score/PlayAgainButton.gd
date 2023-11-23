332c09b30ee7ef9505d713f032465abb2c17a6b3
Jason Wu <45532884+wu-json@users.noreply.github.com>
2023-11-23 18:53:02 -0500

    feat: add score screen skeleton (#6)
    * feat: play button
    
    * feat: play again button
    
    * feat: score placeholder
    
    * feat: score screen

--- scenes/Score/PlayAgainButton.gd
+++ scenes/Score/PlayAgainButton.gd
@@ -1,7 +1,13 @@
 extends TextureButton
 
-func _ready():
-	position = Vector2(30, 30)
-
 func _pressed():
 	SceneSwitcher.change_scene("res://scenes/Play/Play.tscn")
+
+
+func _on_mouse_entered():
+	self.scale = Vector2(1.02, 1.05)
+	self.modulate = Color(1, 0.9, 1 , 1) 
+
+func _on_mouse_exited():
+	self.scale = Vector2(1.0, 1.0)
+	self.modulate = Color(1, 1, 1 , 1) 
--- scenes/Score/Score.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-func _ready():
-	var score_param = SceneSwitcher.get_param("score")
-	var score: int = score_param if score_param != null else 69420
-	$ScoreLabel.set_text(str(score))
