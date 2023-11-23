57053f5d13614538b8a64004a0d31cba6a47e0e1
wu-json <jason.wu2153@gmail.com>
2023-11-23 13:58:14 -0500

    fix: warning

--- autoload/scene_switcher.gd
+++ autoload/scene_switcher.gd
@@ -6,7 +6,7 @@ func change_scene(next_scene, params=null):
 	_params = params
 	get_tree().change_scene_to_file(next_scene)
 	
-func get_param(name):
-	if _params != null and _params.has(name):
-		return _params[name]
+func get_param(param_name):
+	if _params != null and _params.has(param_name):
+		return _params[param_name]
 	return null
