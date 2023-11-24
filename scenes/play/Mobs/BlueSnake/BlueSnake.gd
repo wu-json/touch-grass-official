eba096fb73de0c3c1b1db30b829a055a79da0ac5
wu-json <jason.wu2153@gmail.com>
2023-11-24 15:08:05 -0500

    feat: all sfx

--- scenes/play/Mobs/BlueSnake/BlueSnake.gd
+++ scenes/play/Mobs/BlueSnake/BlueSnake.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+@onready var sfx_wrong = SFX.get_node("WrongBuzzer")
+
 signal blue_snake_pressed
 
 func on_blue_snake_pressed():
+	sfx_wrong.play()
 	blue_snake_pressed.emit()
--- scenes/play/Mobs/Purple/Purple.gd
+++ scenes/play/Mobs/Purple/Purple.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
+@onready var sfx_wrong = SFX.get_node("WrongBuzzer")
+
 signal purple_pressed
 
 func on_purple_pressed():
+	sfx_wrong.play()
 	purple_pressed.emit()
