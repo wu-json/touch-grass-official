f9e8ba1842d45249c93883001f9d4c996c962c85
wu-json <jason.wu2153@gmail.com>
2023-11-24 15:57:38 -0500

    fix: typos

--- scenes/Dialogue/Dialogue.gd
+++ scenes/Dialogue/Dialogue.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 const DIALOGUE = [
 	"Hey there. I'm Capy...",
-	"I'm a capybarra, but I'm not very social...",
+	"I'm a capybara, but I'm not very social...",
 	"I like to hang out in this pond by myself and play games and watch anime on my phone...",
-	"But the other day another capybarra told me to \"touch some grass\".",
+	"But the other day another capybara told me to \"touch some grass\".",
 	"I thought I would give it a try since I'm feeling kind of down these days...",
 	"Do you mind helping me out? All you need to do is touch as much grass as possible while avoiding other distractions.",
 	"Awesome...thank you. Let's get started then..."
